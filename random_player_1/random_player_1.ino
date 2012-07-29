904917301c0487e82ddcf089b7524ec1dd2639f5
Tom Whitwell <tom.whitwell+github@gmail.com>
2012-07-29 21:38:01 +0100

    Note length system
    Add more modes
    Tidy up noteOn function
    Add noteKill function to end notes

--- random_player_1/random_player_1.ino
+++ random_player_1/random_player_1.ino
@@ -5,14 +5,12 @@ Random piano sequencer
  - sequencer looping
  
  Knobs: 
- 
  0 = tempo
  3 = tweaks +/- 2 notes on playback, positiion = probability 
  5 & 6 = min and max velocity
  
  
- Schematic:  
- 
+Schematic in pot_box_schematic 
  
  */
 
@@ -24,12 +22,16 @@ GLOBAL VARIABLES
 byte modechoice;
 byte noteplay;
 #define arraysize 128
-#define modecount 8
+#define modecount 14
 int tempo  = 60;
 int maxtempo=0;
 int mintempo = 200;
 boolean fill=true;
 boolean debug = true; 
+#define greenLED2 44
+#define greenLED1 45
+#define redLED 46
+#define numvoice 32 // maximum polyphony 
 
 /*
 LOOKUP TABLES 
@@ -45,6 +47,16 @@ byte notes[modecount][12]= {
   { 0,2,3,5,7,8,11,12    }, // harmonic minor 
   {0,2,4,5,6,8,10,12    }, // arabian 
   { 0,2,4,5,8,9,11,12    }, // harmonic major 
+  { 0,1,4,5,6,8,11,12    }, // persian 
+  {  0,1,4,5,6,9,10,12   }, // oriental 
+  {   0,1,4,5,7,8,10,12  }, // jewish 
+  {   0,1,4,5,7,8,11,12  }, //  gypsy
+  {   0,2,3,5,7,9,10,12  }, // dorian 
+  {  0,2,4,5,7,8,11,12   }, // ethiopian 
+ 
+//  {     }, //  
+  
+  
 };  
 
 byte odds[arraysize]={
@@ -52,7 +64,7 @@ byte odds[arraysize]={
 //90,10,10,10,90,10,10,10,90,10,10,10,90,10,10,10,90,10,10,10,90,10,10,10,90,10,10,10,90,10,10,10
 //95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5
 95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5
-
+//95,5,95,5,95,5,95,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,5,5,5,5,95,5,5,5,5,5,5,5,95,5,5,5,5,5,5,5,95,5,5,5,5,5,5,5,95,95,95,95,95,95,95,95,95,5,95,5,95,5,95,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,5,5,5,5,95,5,5,5,5,5,5,5,95,5,5,5,5,5,5,5,95,5,5,5,5,5,5,5,95,95,95,95,95,95,95,95,
 
 
 //50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,
@@ -61,13 +73,23 @@ byte odds[arraysize]={
 //90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,20,20
 };
 
+// holder for the sequence 
 byte sequence[2][arraysize];
+
+// arrays to manage note length/endings 
+byte onNote[numvoice];
+unsigned long offTime[numvoice];
+unsigned long time;
 
 void setup() {
   //  Set MIDI baud rate:
 Serial.begin(28800); // debug 
   Serial1.begin(31250); // midi 
    randomSeed(analogRead(9));
+ pinMode(redLED,OUTPUT);
+ pinMode(greenLED1,OUTPUT);
+ pinMode(greenLED2,OUTPUT);
+ 
 }
 
 
@@ -78,7 +100,6 @@ MAIN LOOP
 void loop() {
 
 
-  
   
 
 
@@ -108,12 +129,56 @@ else{
 // AFTER FIRST FILL, STEP THROUGH THE SEQUENCE  
   
 for(int seqstep=0;seqstep<arraysize;seqstep++){    
-        noteOn(0x90, sequence[0][seqstep],sequence[1][seqstep]);
+byte randomNote = 0;
+
+// ***check pots 
+// pot zero = tempo 
+tempo = map(analogRead(0),0,1024,mintempo,maxtempo);
+  
+  // randomise notes, outside the quantisation, with pot 2
+  if (analogRead(2)>random(1024)){
+   randomNote = 2+random(4);
+  if (debug==true){Serial.println("TWEAK");}; 
+  }
+ 
+ 
+ byte velocity = sequence[1][seqstep];
+ if (velocity>0){
+  byte minvelocity = map(analogRead(4),0,1024,1,127);
+  byte maxvelocity = map(analogRead(5),0,1024,minvelocity,127);
+ if (velocity<=minvelocity){velocity=minvelocity;};
+if (velocity>=maxvelocity){velocity=maxvelocity;};
+ }
+ 
+ // PLAY THE NOTE 
+ 
+ // temporarily attach note duration to pot 3 for testing, or to tempo 
+// int notelength = analogRead(3);
+// int notelength = tempo;
+// int notelength = tempo+(analogRead(3)/2);
+// int notelength = seqstep*10;
+//int notelength = (127-velocity)*10;
+//int notelength = (127-sequence[0][seqstep])*10;
+int notelength = tempo*2; 
+
+ 
+ 
+  noteOn(0x90, sequence[0][seqstep]+randomNote,velocity,notelength);
         
-          tempo = map(analogRead(0),0,1024,mintempo,maxtempo);
+
+
 //  if (debug == true){Serial.print("tempo=");Serial.println(tempo);};
+
+if (seqstep%4==0){
+digitalWrite(greenLED1, HIGH);
+}
+else{
+digitalWrite(greenLED1, LOW);
+}
         
+ noteKill();
 delay(tempo);
+
 }  
 
 byte changer=random(arraysize);
@@ -123,8 +188,7 @@ sequence[0][changer]= noteplay;
 sequence[1][changer]= random(odds[changer]+27);
 }
  
-noteOn(0x90,quantize(modechoice, random(8),random(4)+4),0);
-  
+ 
 }
 
 
@@ -134,34 +198,29 @@ FUNCTIONS
 
 // midi note player 
 
-//  plays a MIDI note.  Doesn't check to see that
-//  cmd is greater than 127, or that data values are  less than 127:
-void noteOn(int cmd, int pitch, int velocity) {
+void noteOn(byte cmd, byte pitch, byte velocity, int duration) {
+time = millis();
+// find an empty note slot 
+boolean foundslot = false;
+for (int i=0;i<numvoice;i++){
+if (onNote[i] == 0){
   
-  // note scale break, with pot 2
-  if (analogRead(2)>random(1024) && velocity>0){
-   pitch = pitch-2+random(4);
-  if (debug==true){Serial.println("TWEAK");}; 
-  }
-
-if (velocity>0){  
-  byte minvelocity = map(analogRead(4),0,1024,1,127);
-  byte maxvelocity = map(analogRead(5),0,1024,minvelocity,127);
-    if (debug==true){
-    Serial.print("minvelocity=");
-    Serial.print(minvelocity);
-    Serial.print(" maxvelocity=");
-    Serial.println(maxvelocity);
-    
-    Serial.print("pre_velocity=");Serial.print(velocity);}; 
-if (velocity<=minvelocity){velocity=minvelocity;};
-if (velocity>=maxvelocity){velocity=maxvelocity;};
-    if (debug==true){Serial.print(" post_velocity=");Serial.println(velocity);}; 
+  onNote[i] = pitch;
+  offTime[i] = time+duration;
+  foundslot = true;
+  
+  break;
 }
+}
+
+// RED LIGHT SHOWS NOTE BUFFER IS OVERFULL 
+if (foundslot == false){digitalWrite (redLED, HIGH);}
+else{digitalWrite (redLED, LOW);};
   
   Serial1.write(cmd);
   Serial1.write(pitch);
   Serial1.write(velocity);
+
   
   if (debug == true && velocity>0){  
   Serial.print("Pitch ");
@@ -170,6 +229,23 @@ if (velocity>=maxvelocity){velocity=maxvelocity;};
   Serial.println(velocity);
   }
 }
+
+// Check for held notes that should be killed, and kill them. 
+
+void noteKill(){
+time=millis();
+for (int i=0;i<numvoice;i++){
+if (onNote[i]>0 && offTime[i]<time){  // if note value>0 and note off time < time
+ 
+ byte pitch = onNote[i];
+ byte velocity = 0;
+  Serial1.write(0x90);
+  Serial1.write(pitch);
+  Serial1.write(velocity);
+  
+      onNote[i]=0; //wipe onNote entry to signal no active note
+}}}
+
 
 // Quantizer 
 

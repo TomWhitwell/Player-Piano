21b5cd236088e99c464bbd1e390145e6d55523bf
Tom Whitwell <tom.whitwell+github@gmail.com>
2012-08-02 10:16:50 +0100

    Quantize notes as they're played, add knob to change scales on playback

--- random_player_1/random_player_1.ino
+++ random_player_1/random_player_1.ino
@@ -9,7 +9,7 @@ Random piano sequencer
  1 = number of notes changed each 128 note cycle
  2 = tweaks +/- 2 notes on playback, positiion = probability 
  5 & 6 = min and max velocity
- 
+ 3 = loop length 
  
 Schematic in pot_box_schematic 
  
@@ -29,6 +29,7 @@ int maxtempo=0;
 int mintempo = 200;
 boolean fill=true;
 boolean debug = false; 
+boolean Display = true; 
 #define greenLED2 44
 #define greenLED1 45
 #define redLED 46
@@ -37,7 +38,7 @@ boolean debug = false;
 #define oddsCount 6
 byte oddsChoice;
 byte LongShort;
-byte BaseTime;
+byte BaseTime =2;
 
 /*
 LOOKUP TABLES 
@@ -90,7 +91,7 @@ byte odds[oddsCount][arraysize]={
 }};
 
 // holder for the sequence 
-byte sequence[2][arraysize];
+byte sequence[3][arraysize]; //0=note 1=octave 2=velocity  
 
 // arrays to manage note length/endings 
 byte onNote[numvoice];
@@ -141,12 +142,14 @@ BaseTime = random(8);
 
 
 // FILL THE LOOP 
-for(int seqstep=0;seqstep<arraysize;seqstep++){    
-byte a=random(odds[oddsChoice][seqstep])/14;
-       noteplay=quantize(modechoice, a, random(4)+4);
-if(random(density)<odds[oddsChoice][seqstep]){
-sequence[0][seqstep]= noteplay;
-sequence[1][seqstep]= random(odds[oddsChoice][seqstep]+27);
+for(int seqstep=0;seqstep<arraysize;seqstep++){
+ 
+    byte newnote=random(7);
+    byte newoctave = random(5)+3;
+    if(random(density)<odds[oddsChoice][seqstep]){
+    sequence[0][seqstep]= newnote; // note 
+    sequence[1][seqstep]= newoctave;  // octave 
+    sequence[2][seqstep]= random(odds[oddsChoice][seqstep]+27); // velocity 
 
 }
 }
@@ -165,23 +168,35 @@ byte randomNote = 0;
 // ROUTINES TO CHECK THE KNOBS  
 // pot zero = tempo 
 tempo = map(analogRead(0),0,1024,mintempo,maxtempo);
-  
+ 
+ // Knob 2
+ 
   // RANDOMISE KNOB 
-  if (analogRead(2)>random(1024)){
-   randomNote = 2+random(4);
-  if (debug==true){Serial.println("TWEAK");}; 
-  }
+//  if (analogRead(2)>random(1024)){
+//   randomNote = -2+random(4);
+//  if (debug==true){Serial.println("TWEAK");}; 
+//  }
+//  
+// SCALE SELECTION KNOB 
+  modechoice = map(analogRead(2),0,1024,0,modecount);
+ 
+// byte octave_override = map(analogRead(2),0,1024,0,10)+2;
+//  byte note_override = map(analogRead(2),0,1024,0,7);
+
  
  // VELOCITY HIGH/LOW KNOBS 
- byte velocity = sequence[1][seqstep];
+ byte velocity = sequence[2][seqstep];
  if (velocity>0){
   byte minvelocity = map(analogRead(4),0,1024,1,127);
   byte maxvelocity = map(analogRead(5),0,1024,minvelocity,127);
  if (velocity<=minvelocity){velocity=minvelocity;};
 if (velocity>=maxvelocity){velocity=maxvelocity;};
  }
- // CHANGE KNOB 
+
+// // CHANGE KNOB 
 noteschange = map(analogRead(1),0,1024,0,arraysize); 
+
+
  
  // LOOP KNOB 
  playsteps = (analogRead(3)/128)*8;
@@ -197,13 +212,19 @@ noteschange = map(analogRead(1),0,1024,0,arraysize);
 //int notelength = (127-sequence[0][seqstep])*10;
 //int notelength = tempo*2; 
 int notelength; 
-if (seqstep % LongShort == 0){notelength = random(2000)+tempo;} // play on specific note divisions 
+if (seqstep % LongShort == 0){notelength = random(8)*tempo;} // play on specific note divisions 
 else {notelength = tempo*BaseTime;};
 
  
  
-  noteOn(0x90, sequence[0][seqstep]+randomNote,velocity,notelength);
-        
+  noteOn(0x90, quantize(modechoice, sequence[0][seqstep], sequence[1][seqstep])+randomNote,velocity,notelength);
+ 
+ // OCTAVE OVERRIDE VERSION 
+//   noteOn(0x90, quantize(modechoice, sequence[0][seqstep], octave_override)+randomNote,velocity,notelength);
+       
+ // OCTAVE OVERRIDE VERSION 
+//   noteOn(0x90, quantize(modechoice, note_override, sequence[1][seqstep])+randomNote,velocity,notelength);
+ 
 
 
 //  if (debug == true){Serial.print("tempo=");Serial.println(tempo);};
@@ -221,19 +242,30 @@ delay(tempo);
 }  
 
 
-// CHANGES BETWEEN ZERO AND ALL NOTES EACH CYCLE 
+// MAKE CHANGES 
 
 for (int i = 0; i<noteschange; i++){
-byte changer=random(arraysize);
-byte a=random(odds[oddsChoice][changer])/14;
-noteplay=quantize(modechoice, a, random(4)+4);
-if(random(density)<odds[oddsChoice][changer]){
-sequence[0][changer]= noteplay;
-sequence[1][changer]= random(odds[oddsChoice][changer]+27);
+byte changer=random(arraysize); // choose an entry to change 
+if(random(density)<odds[oddsChoice][changer] && sequence[2][changer]>0){ // if a rhythmically interesting note and non-silent 
+sequence[0][changer] = sequence[0][changer]+(random(3)-1);
+
 }
 }
+
+//for (int i = 0; i<noteschange; i++){
+//byte changer=random(arraysize);
+//byte a=random(odds[oddsChoice][changer])/14;
+//noteplay=quantize(modechoice, a, random(4)+4);
+//if(random(density)<odds[oddsChoice][changer]){
+//sequence[0][changer]= noteplay;
+//sequence[1][changer]= random(odds[oddsChoice][changer]+27);
+//}
+//}
+
+
+
 } 
- 
+
 }
 
 

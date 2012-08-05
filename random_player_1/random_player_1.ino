dc8d0b9e45e1815d41689172021055ad5a4b71d3
Tom Whitwell <tom.whitwell+github@gmail.com>
2012-08-05 19:30:14 +0100

    rationalising variable names

--- random_player_1/random_player_1.ino
+++ random_player_1/random_player_1.ino
@@ -1,12 +1,12 @@
 /*
-Random piano sequencer 
+Random piano Sequencer 
  
  Notes on this version: 
- - sequencer looping
+ - Sequencer looping
  
  Knobs: 
- 0 = tempo
- 1 = number of notes changed each 128 note cycle
+ 0 = TEMPO
+ 1 = number of MODES changed each 128 note cycle
  2 = mode selection 
  5 & 6 = min and max velocity
  3 = loop length 
@@ -23,17 +23,25 @@ Create collection of note change functions;
 	add new note (random note/velocity/duration) 
 	add new note (note/velocity/duration related to previous note(s) - markov)
 	Add new note (note/velocity/duration related to rhythm system)
-	Change all notes above/below a specific velocity 
+	Change all MODES above/below a specific velocity 
 Create standard internal numbering system (i.e. 0-256) that can be applied to any variable.
 	ie create a LFO which can be applied to note/velocity/duration 
 	build markov chain around this variable
 		so you can repeat/learn velocity chains as easily as note chains 
 	BUT: How to connect 0-256 number with 0-7+0-8 note+octave system? 
 System to change a particular global variable for a period 
-	i.e. change base note / mode / tempo for n bars, then revert 
-	Is it a temporary change vs a permanent change - maybe with a buffer to reverse the change? 
+	i.e. change base note / mode / TEMPO for n bars, then revert 
+	Is it a TEMPOrary change vs a permanent change - maybe with a buffer to reverse the change? 
 Rework rhythm system - enable changes of rhythm 
-	(Could be covered by note change function above, i.e. ChangeVelocityByRhythm) 
+	(Could be covered by note change function above, i.e. ChangeVelocityByRhythm)
+Rework timing system. 
+        Use 24ppq system, to enable midi clock output? 
+Rework Quantise system to ensure it correctly handles differently-sized scales
+Rework Rhythm system to use smaller arrays 
+Create chord system, scanning current notes playing and running chords over the top
+
+Experiments to try: 
+        Create change systems for note/velocity/duration and attach them to knobs 
 
 
 	
@@ -44,32 +52,51 @@ Rework rhythm system - enable changes of rhythm
 /*
 GLOBAL VARIABLES 
 */
-byte modechoice;
+byte MODE_CHOICE;
+#define MODE_COUNT 14
+
+byte ODDS_CHOICE;
+#define ODDS_COUNT 6
+
 byte noteplay;
-#define arraysize 128
-#define modecount 14
-int tempo  = 60;
-int maxtempo=0;
-int mintempo = 200;
-boolean fill=true;
-boolean debug = false; 
-boolean Display = true; 
+#define ARRAY_SIZE 128
+
+int TEMPO  = 60;
+int MAX_TEMPO=0;
+int MIN_TEMPO = 200;
+
+int DENSITY =100; // 100 = average, 200 = low, 50 = high
+int MAX_DENSITY = 200;
+int MIN_DENSITY = 10;
+
+boolean FILL=true;
+boolean DEBUG = false; 
+boolean PRINT_SETTINGS = true; 
+
 #define greenLED2 44
 #define greenLED1 45
 #define redLED 46
-#define numvoice 16 // maximum polyphony 
-#define density 100 // 100 = average, 200 = low, 50 = high 
-#define oddsCount 6
-byte oddsChoice;
-byte LongShort;
-byte BaseTime =2;
+
+#define VOICE_COUNT 16 // maximum polyphony 
+
+byte LONG_SHORT;
+byte BASE_time; // multiplier for note lengths 
+
+// holder for the SEQUENCE 
+byte SEQUENCE[3][ARRAY_SIZE]; //0=note 1=octave 2=velocity  
+
+// arrays to manage note length/endings 
+byte NOTE_ON[VOICE_COUNT];
+unsigned long NOTE_OFF[VOICE_COUNT];
+
+
 
 /*
 LOOKUP TABLES 
 */
 
 //QUANTISATION 
-byte notes[modecount][12]= {
+byte MODES[MODE_COUNT][12]= {
   {0,1,2,3,4,5,6,7,8,9,10,11    },   // chromatic 
   {0,2,4,6,8,10,12    }, //whole tone 
   { 0,3,5,6,7,10,12    }, // blues 
@@ -99,7 +126,7 @@ byte notes[modecount][12]= {
 5 = bossanova 
 */
 
-byte odds[oddsCount][arraysize]={
+byte ODDS[ODDS_COUNT][ARRAY_SIZE]={
 {
 95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5,95,5,5,5
 },{
@@ -114,19 +141,13 @@ byte odds[oddsCount][arraysize]={
 95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,  
 }};
 
-// holder for the sequence 
-byte sequence[3][arraysize]; //0=note 1=octave 2=velocity  
 
-// arrays to manage note length/endings 
-byte onNote[numvoice];
-unsigned long offTime[numvoice];
-unsigned long time;
 
 void setup() {
   //  Set MIDI baud rate:
-Serial.begin(28800); // debug 
+Serial.begin(28800); // DEBUG 
   Serial1.begin(31250); // midi 
-   randomSeed(analogRead(9));
+   randomSeed((analogRead(9)+analogRead(10)+analogRead(11))/3);
  pinMode(redLED,OUTPUT);
  pinMode(greenLED1,OUTPUT);
  pinMode(greenLED2,OUTPUT);
@@ -141,43 +162,45 @@ MAIN LOOP
 void loop() {
 
 
-  byte noteschange;
-byte playsteps = 16; // how many notes to play in each loop 
+  byte MODESchange;
+byte playsteps = 16; // how many MODES to play in each loop 
 
 
 // FIRST PLAY, FILL UP THE SEQUENCE  
-if(fill == true){
+if(FILL == true){
 
   // CHOOSE INITIAL VARIABLES 
   
   // Random or fixed scale selection 
-modechoice=random(modecount);
-//modechoice=3;
+MODE_CHOICE=random(MODE_COUNT);
+//MODE_CHOICE=3;
 
 // Random or fixed rhythm selection 
-oddsChoice = random(oddsCount);
-//modechoice = 4;
+ODDS_CHOICE = random(ODDS_COUNT);
+//MODE_CHOICE = 4;
 
+// select density
+DENSITY = (random(MAX_DENSITY-MIN_DENSITY))+MIN_DENSITY;
 
 //EXPERIMENTAL 
 // Choose divider for long/short stresses 
-LongShort = random(16);
-BaseTime = random(8);
+LONG_SHORT = random(16);
+BASE_time = random(16);
 
 
 // FILL THE LOOP 
-for(int seqstep=0;seqstep<arraysize;seqstep++){
+for(int seqstep=0;seqstep<ARRAY_SIZE;seqstep++){
  
     byte newnote=random(7);
     byte newoctave = random(5)+3;
-    if(random(density)<odds[oddsChoice][seqstep]){
-    sequence[0][seqstep]= newnote; // note 
-    sequence[1][seqstep]= newoctave;  // octave 
-    sequence[2][seqstep]= random(odds[oddsChoice][seqstep]+27); // velocity 
+    if(random(DENSITY)<ODDS[ODDS_CHOICE][seqstep]){
+    SEQUENCE[0][seqstep]= newnote; // note 
+    SEQUENCE[1][seqstep]= newoctave;  // octave 
+    SEQUENCE[2][seqstep]= random(ODDS[ODDS_CHOICE][seqstep]+27); // velocity 
 
 }
 }
-fill = false;
+FILL = false;
 }
 
 
@@ -190,26 +213,26 @@ for(int seqstep=0;seqstep<playsteps;seqstep++){
 byte randomNote = 0;
 
 // ROUTINES TO CHECK THE KNOBS  
-// pot zero = tempo 
-tempo = map(analogRead(0),0,1024,mintempo,maxtempo);
+// pot zero = TEMPO 
+TEMPO = map(analogRead(0),0,1024,MIN_TEMPO,MAX_TEMPO);
  
  // Knob 2
  
   // RANDOMISE KNOB 
 //  if (analogRead(2)>random(1024)){
 //   randomNote = -2+random(4);
-//  if (debug==true){Serial.println("TWEAK");}; 
+//  if (DEBUG==true){Serial.println("TWEAK");}; 
 //  }
 //  
 // SCALE SELECTION KNOB 
-  modechoice = map(analogRead(2),0,1024,0,modecount);
+  MODE_CHOICE = map(analogRead(2),0,1024,0,MODE_COUNT);
  
 // byte octave_override = map(analogRead(2),0,1024,0,10)+2;
 //  byte note_override = map(analogRead(2),0,1024,0,7);
 
  
  // VELOCITY HIGH/LOW KNOBS 
- byte velocity = sequence[2][seqstep];
+ byte velocity = SEQUENCE[2][seqstep];
  if (velocity>0){
   byte minvelocity = map(analogRead(4),0,1024,1,127);
   byte maxvelocity = map(analogRead(5),0,1024,minvelocity,127);
@@ -218,7 +241,7 @@ if (velocity>=maxvelocity){velocity=maxvelocity;};
  }
 
 // // CHANGE KNOB 
-noteschange = map(analogRead(1),0,1024,0,arraysize); 
+MODESchange = map(analogRead(1),0,1024,0,ARRAY_SIZE); 
 
 
  
@@ -227,31 +250,31 @@ noteschange = map(analogRead(1),0,1024,0,arraysize);
  
  // PLAY THE NOTE 
  
- // temporarily attach note duration to pot 3 for testing, or to tempo 
+ // TEMPOrarily attach note duration to pot 3 for testing, or to TEMPO 
 // int notelength = analogRead(3);
-// int notelength = tempo;
-// int notelength = tempo+(analogRead(3));
+// int notelength = TEMPO;
+// int notelength = TEMPO+(analogRead(3));
 // int notelength = seqstep*10;
 //int notelength = (127-velocity)*10;
-//int notelength = (127-sequence[0][seqstep])*10;
-//int notelength = tempo*2; 
+//int notelength = (127-SEQUENCE[0][seqstep])*10;
+//int notelength = TEMPO*2; 
 int notelength; 
-if (seqstep % LongShort == 0){notelength = random(8)*tempo;} // play on specific note divisions 
-else {notelength = tempo*BaseTime;};
+if (seqstep % LONG_SHORT == 0){notelength = random(8)*TEMPO;} // play on specific note divisions 
+else {notelength = TEMPO*BASE_time;};
 
  
  
-  noteOn(0x90, quantize(modechoice, sequence[0][seqstep], sequence[1][seqstep])+randomNote,velocity,notelength);
+  playNote(0x90, quantize(MODE_CHOICE, SEQUENCE[0][seqstep], SEQUENCE[1][seqstep])+randomNote,velocity,notelength);
  
  // OCTAVE OVERRIDE VERSION 
-//   noteOn(0x90, quantize(modechoice, sequence[0][seqstep], octave_override)+randomNote,velocity,notelength);
+//   playNote(0x90, quantize(MODE_CHOICE, SEQUENCE[0][seqstep], octave_override)+randomNote,velocity,notelength);
        
  // OCTAVE OVERRIDE VERSION 
-//   noteOn(0x90, quantize(modechoice, note_override, sequence[1][seqstep])+randomNote,velocity,notelength);
+//   playNote(0x90, quantize(MODE_CHOICE, note_override, SEQUENCE[1][seqstep])+randomNote,velocity,notelength);
  
 
 
-//  if (debug == true){Serial.print("tempo=");Serial.println(tempo);};
+//  if (DEBUG == true){Serial.print("TEMPO=");Serial.println(TEMPO);};
 
 if (seqstep%4==0){
 digitalWrite(greenLED1, HIGH);
@@ -261,28 +284,28 @@ digitalWrite(greenLED1, LOW);
 }
         
  noteKill();
-delay(tempo);
+delay(TEMPO);
 
 }  
 
 
 // MAKE CHANGES 
 
-for (int i = 0; i<noteschange; i++){
-byte changer=random(arraysize); // choose an entry to change 
-if(random(density)<odds[oddsChoice][changer] && sequence[2][changer]>0){ // if a rhythmically interesting note and non-silent 
-sequence[0][changer] = sequence[0][changer]+(random(3)-1);
+for (int i = 0; i<MODESchange; i++){
+byte changer=random(ARRAY_SIZE); // choose an entry to change 
+if(random(DENSITY)<ODDS[ODDS_CHOICE][changer] && SEQUENCE[2][changer]>0){ // if a rhythmically interesting note and non-silent 
+SEQUENCE[0][changer] = SEQUENCE[0][changer]+(random(3)-1);
 
 }
 }
 
-//for (int i = 0; i<noteschange; i++){
-//byte changer=random(arraysize);
-//byte a=random(odds[oddsChoice][changer])/14;
-//noteplay=quantize(modechoice, a, random(4)+4);
-//if(random(density)<odds[oddsChoice][changer]){
-//sequence[0][changer]= noteplay;
-//sequence[1][changer]= random(odds[oddsChoice][changer]+27);
+//for (int i = 0; i<MODESchange; i++){
+//byte changer=random(ARRAY_SIZE);
+//byte a=random(ODDS[ODDS_CHOICE][changer])/14;
+//noteplay=quantize(MODE_CHOICE, a, random(4)+4);
+//if(random(density)<ODDS[ODDS_CHOICE][changer]){
+//SEQUENCE[0][changer]= noteplay;
+//SEQUENCE[1][changer]= random(ODDS[ODDS_CHOICE][changer]+27);
 //}
 //}
 
@@ -299,23 +322,23 @@ FUNCTIONS
 
 // midi note player 
 
-void noteOn(byte cmd, byte pitch, byte velocity, int duration) {
-time = millis();
+void playNote(byte cmd, byte pitch, byte velocity, int duration) {
+unsigned long time = millis();
 // find an empty note slot 
-boolean foundslot = false;
-for (int i=0;i<numvoice;i++){
-if (onNote[i] == 0){
+boolean found_slot = false;
+for (int i=0;i<VOICE_COUNT;i++){
+if (NOTE_ON[i] == 0){
   
-  onNote[i] = pitch;
-  offTime[i] = time+duration;
-  foundslot = true;
+  NOTE_ON[i] = pitch;
+  NOTE_OFF[i] = time+duration;
+  found_slot = true;
   
   break;
 }
 }
 
 // RED LIGHT SHOWS NOTE BUFFER IS OVERFULL 
-if (foundslot == false){digitalWrite (redLED, HIGH);}
+if (found_slot == false){digitalWrite (redLED, HIGH);}
 else{digitalWrite (redLED, LOW);};
   
   Serial1.write(cmd);
@@ -323,7 +346,7 @@ else{digitalWrite (redLED, LOW);};
   Serial1.write(velocity);
 
   
-  if (debug == true && velocity>0){  
+  if (DEBUG == true && velocity>0){  
   Serial.print("Pitch ");
   Serial.print(pitch);
   Serial.print(" Velocity ");
@@ -331,27 +354,27 @@ else{digitalWrite (redLED, LOW);};
   }
 }
 
-// Check for held notes that should be killed, and kill them. 
+// Check for held Notes that should be killed, and kill them. 
 
 void noteKill(){
-time=millis();
-for (int i=0;i<numvoice;i++){
-if (onNote[i]>0 && offTime[i]<time){  // if note value>0 and note off time < time
+unsigned long time=millis();
+for (int i=0;i<VOICE_COUNT;i++){
+if (NOTE_ON[i]>0 && NOTE_OFF[i]<time){  // if note value>0 and note off time < time
  
- byte pitch = onNote[i];
+ byte pitch = NOTE_ON[i];
  byte velocity = 0;
   Serial1.write(0x90);
   Serial1.write(pitch);
   Serial1.write(velocity);
   
-      onNote[i]=0; //wipe onNote entry to signal no active note
+      NOTE_ON[i]=0; //wipe NOTE_ON entry to signal no active note
 }}}
 
 
 // Quantizer 
 
 byte quantize(int scale, int note, int octave){
-  return octave*12+notes[scale][note]; 
+  return octave*12+MODES[scale][note]; 
 }
 
 

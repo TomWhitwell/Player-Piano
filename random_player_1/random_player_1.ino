1a7b442e5a0b4232e875dfeda1a417c6af952edf
Tom Whitwell <tom.whitwell+github@gmail.com>
2012-08-18 23:12:22 +0100

    Start to add functions to add/change/increment notes
    Also add system to show names of chosen options

--- random_player_1/random_player_1.ino
+++ random_player_1/random_player_1.ino
@@ -60,7 +60,7 @@ byte MODE_CHOICE;
 byte ODDS_CHOICE;
 byte noteplay;
 int TEMPO  = 120;
-int MAX_TEMPO=300;
+int MAX_TEMPO=400;
 int MIN_TEMPO = 8;
 int PERIOD; 
 int DENSITY =100; // 100 = average, 200 = low, 50 = high
@@ -70,7 +70,6 @@ byte MIN_VELOCITY;
 byte MAX_VELOCITY;
 int LOOP_LENGTH = ARRAY_SIZE;
 int CHANGES = 0;
-boolean FILL=true;
 boolean DEBUG = false; 
 boolean PRINT_SETTINGS = true; 
 #define greenLED2 44
@@ -86,6 +85,12 @@ long loop_count = 0;
 
 long OLD;
 long NEW;
+
+// FLOW CONTROL 
+boolean START = true; 
+boolean FILL = false;
+boolean NOTE = false;
+boolean LOOP = false; 
 
 
 
@@ -106,7 +111,7 @@ int SEQUENCE_STEP;
 //QUANTISATION
 #define MODE_COUNT 18 
 byte MODES[MODE_COUNT][12]= {
-  {0,1,2,3,4,5,6,7,8,9,10,11},// chromatic 
+  {0,2,4,6,8,10,12},          // whole tone 
   {0,2,4,5,7,9,11,12},        // major 
   {0,2,4,5,8,9,11,12},        // harmonic major 
   {0,2,4,7,9,12,14},          // pentatonic major 
@@ -114,7 +119,6 @@ byte MODES[MODE_COUNT][12]= {
   {0,2,3,5,7,8,11,12},        // harmonic minor
   {0,3,5,7,10,12,15},         // pentatonic minor  
   {0,2,3,5,6,8,9,11},         // diminished
-  {0,2,4,6,8,10,12},          // whole tone 
   {0,3,5,6,7,10,12},          // blues 
   {0,1,3,5,7,9,10,12},        // javanese
   {0,2,4,5,6,8,10,12},        // arabian 
@@ -126,6 +130,25 @@ byte MODES[MODE_COUNT][12]= {
   {0,2,4,5,7,8,11,12},        // ethiopian 
 };  
 
+char MODE_NAMES[MODE_COUNT][12]=
+{
+ "Whole Tone",
+"Major",
+"HarmMajor",
+"PentMajor",
+"Minor",
+"HarmMinr",
+"PentMinor",
+"Diminished",
+"Blues",
+"Java",
+"Arabian",
+"Persian",
+"Oriental",
+"Jewish",
+"Gypsy",
+"Dorian",
+"Ethiopian"};
 
 #define ODDS_COUNT 6
 byte ODDS[ODDS_COUNT][ARRAY_SIZE]={
@@ -143,7 +166,13 @@ byte ODDS[ODDS_COUNT][ARRAY_SIZE]={
 {90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,90,20,20,20,20},
 {95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,95,5,95,95,5,95,95,5,},
 {95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,95,5,5,95,5,5,95,5,5,5,95,5,5,95,5,5,}};
-
+char ODDS_NAMES[ODDS_COUNT][15]=
+  {"4/4",
+  "clock divns",
+  "samba",
+  "3/4",
+  "cuban",
+  "Bossanova"};
 
 
 //*************
@@ -170,35 +199,37 @@ void setup() {
 
 void loop() {
 
+if (loop_count%CONTROL_REFRESH == 0){  
+readKnobs();}
+
+
+if(START == true){
+randomiseValues();
+START = false;
+}
+
 
 if(FILL == true){
-randomiseValues();
 fillRandom();
 FILL = false;
 }
+
+if (NOTE == true){
   
-if (loop_count%CONTROL_REFRESH == 0){  
-readKnobs();
+  
+  
+NOTE = false;  
 }
 
 
-
-// EVERY 96 NOTES, FREEZE AND REFILL  
-  if(REPEAT_COUNT==96){  
-   freeze();
-    fillRandom();}
-    if(REPEAT_COUNT == 192){
-      unFreeze();
-      REPEAT_COUNT = 0;};
-
-
-// MAKE CHANGES EVERY 128 NOTES **NEED TO FIX THIS, WILL RUN EVERY NOTE, CAN SIMPLIFY TO CHANGE ONE NOTE EACH CYCLE 
-//
-//for (int i = 0; i<CHANGES; i++){
-//byte changer=random(ARRAY_SIZE); // choose an entry to change 
-//if(random(DENSITY)<ODDS[ODDS_CHOICE][changer] && SEQUENCE[2][changer]>0){ // if a rhythmically interesting note and non-silent 
-//SEQUENCE[0][changer] = SEQUENCE[0][changer]+(random(3)-1);}}
-
+if (LOOP == true){
+  addRandomNoteAt(random(LOOP_LENGTH));
+//  changeRandomNoteAt(random(LOOP_LENGTH));
+  incrementNoteAt(random(LOOP_LENGTH));
+  decrementNoteAt(random(LOOP_LENGTH));
+  
+LOOP = false;  
+}
 
 
 
@@ -211,6 +242,9 @@ loop_count++;
 //*************
 //FUNCTIONS 
 //*************
+
+
+
 
 // midi note player 
 
@@ -283,13 +317,72 @@ digitalWrite(greenLED2, LOW);
 
 void fillRandom(){
 for(int fill_step=0;fill_step<ARRAY_SIZE;fill_step++){
+    if(random(DENSITY)<ODDS[ODDS_CHOICE][fill_step]){
     byte newnote=random(7);
     byte newoctave = random(5)+3;
-    if(random(DENSITY)<ODDS[ODDS_CHOICE][fill_step]){
     SEQUENCE[0][fill_step]= newnote; // note 
     SEQUENCE[1][fill_step]= newoctave;  // octave 
     SEQUENCE[2][fill_step]= random(ODDS[ODDS_CHOICE][fill_step]+27); // velocity 
 }}}
+
+
+/*
+Document note creation/modification functions: 
+
+addRandomNoteAt(note_position)
+= Checks to see if there is a note present. If not, assesses the rhythm system, throws a dice, and if successful, adds a new random note in the relevant position. 
+
+changeRandomNoteAt(note_position)
+= Checks to see if there is a note present. If there IS, assesses the rhythm system, throws a dice, and if successful, changes to a new random note in the relevant position. 
+
+incrementNoteAt(note_positon)
+= raises note by one, raising octave after 7 notes, looping through octaves 
+*/
+
+void addRandomNoteAt(int note_position){
+  if (note_position<ARRAY_SIZE && SEQUENCE[2][note_position] == 0){
+     if(random(DENSITY)<ODDS[ODDS_CHOICE][note_position]){
+    byte newnote=random(7);
+    byte newoctave = random(5)+3;
+    SEQUENCE[0][note_position]= newnote; // note 
+    SEQUENCE[1][note_position]= newoctave;  // octave 
+    SEQUENCE[2][note_position]= random(ODDS[ODDS_CHOICE][note_position]+27); // velocity 
+}}}
+
+void changeRandomNoteAt(int note_position){
+   if (note_position<ARRAY_SIZE && SEQUENCE[2][note_position] > 0){
+     if(random(DENSITY)<ODDS[ODDS_CHOICE][note_position]){
+    byte newnote=random(7);
+    byte newoctave = random(5)+3;
+    SEQUENCE[0][note_position]= newnote; // note 
+    SEQUENCE[1][note_position]= newoctave;  // octave 
+    SEQUENCE[2][note_position]= random(ODDS[ODDS_CHOICE][note_position]+27); // velocity 
+}}}
+
+
+void incrementNoteAt(int note_position){
+    if (note_position<ARRAY_SIZE && SEQUENCE[2][note_position] > 0){
+    SEQUENCE[0][note_position]++;  
+if (SEQUENCE[0][note_position]>7){
+  SEQUENCE[0][note_position] = 0;
+  SEQUENCE[1][note_position]++;}
+  if (SEQUENCE[1][note_position] > 8){
+    SEQUENCE[1][note_position] = 3;
+}}}
+
+void decrementNoteAt(int note_position){
+    if (note_position<ARRAY_SIZE && SEQUENCE[2][note_position] > 0){
+if (SEQUENCE[0][note_position]==1){
+  SEQUENCE[0][note_position] = 7;
+  SEQUENCE[1][note_position]--;}
+ else {SEQUENCE[0][note_position]--;};
+ 
+  if (SEQUENCE[1][note_position] < 3){
+    SEQUENCE[1][note_position] = 8;
+}}}
+
+
+
 
 //  RETURN VELOCITY WITHIN LIMITS SET BY POTS 
 int getVelocity(int velocity){
@@ -307,13 +400,22 @@ MODE_CHOICE=random(MODE_COUNT);
 ODDS_CHOICE = random(ODDS_COUNT);
 DENSITY = (random(MAX_DENSITY-MIN_DENSITY))+MIN_DENSITY; 
 BASE_TIME = random(16); 
+if (PRINT_SETTINGS == true){
+Serial.print(" Density = ");
+Serial.print(DENSITY);
+Serial.print(" Base Times = ");
+Serial.print(BASE_TIME);}
+Serial.print(" Rhythm = ");
+Serial.print(ODDS_NAMES[ODDS_CHOICE]);
+Serial.print(" Mode = ");
+Serial.println(MODE_NAMES[MODE_CHOICE]);
 }
 
 // READ KNOBS 
 void readKnobs(){
  TEMPO = map(analogRead(0),0,1024,MIN_TEMPO,MAX_TEMPO);
 CHANGES = map(analogRead(1),0,1024,0,ARRAY_SIZE); 
-MODE_CHOICE = map(analogRead(2),0,1024,0,MODE_COUNT);
+//MODE_CHOICE = map(analogRead(2),0,1024,0,MODE_COUNT);
 LOOP_LENGTH = (analogRead(3)/128)*8;
 MIN_VELOCITY = map(analogRead(4),0,1024,1,127);
 MAX_VELOCITY = map(analogRead(5),0,1024,MIN_VELOCITY,127);
@@ -335,15 +437,18 @@ if (PLAY_COUNTER == PLAY_DIVIDER){
 
 byte velocity = getVelocity(SEQUENCE[2][SEQUENCE_STEP]);
 
-int note_length = PERIOD*PLAY_DIVIDER*6;
+int note_length = PERIOD*PLAY_DIVIDER*BASE_TIME;
 byte this_note = quantize(MODE_CHOICE, SEQUENCE[0][SEQUENCE_STEP], SEQUENCE[1][SEQUENCE_STEP]);
 playNote(0x90, this_note,velocity,note_length);
 noteKill();
 if (SEQUENCE_STEP%4==0){digitalWrite(greenLED1, HIGH);}else{digitalWrite(greenLED1, LOW);}
 REPEAT_COUNT++;
 SEQUENCE_STEP++;
+NOTE=true;
 if (SEQUENCE_STEP>LOOP_LENGTH){
  SEQUENCE_STEP = 0; 
+ LOOP = true; 
+ Serial.println("LOOP");
 }
 
 setTimer(TEMPO);
@@ -351,8 +456,6 @@ setTimer(TEMPO);
 PLAY_COUNTER = 0;
 }
 PLAY_COUNTER++;
-
-
 }
 
 
